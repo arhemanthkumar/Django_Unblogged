2f58a3da8133e1422d47c26511de7087fc9b4290
Hemanth <hemanthar95@gmail.com>
2024-11-05 14:18:22 +0530

    Passing of dummy data along with title

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 
+#Dummy data to pass
+posts = [
+    {
+        'author':'Hemanth',
+        'title':'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'November 5'
+    },
+    {
+        'author':'Kumar',
+        'title':'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'November 6'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts,
+        'title': 'Blog Home' # We can also add title here and pass the key in total
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'}) # we can pass the title individually
